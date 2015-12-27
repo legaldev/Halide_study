14236c17540f60061b7593b61a8b2d4dd1f8a9bf
legal <legeldev0@gmail.com>
2015-12-27 12:11:25 +0800

    finish cut head and tail

--- Halide_study/Halide_study/Halide_study.cpp
+++ Halide_study/Halide_study/Halide_study.cpp
@@ -7,13 +7,14 @@
 #include <iostream>
 #include <stdio.h>
 
+
 // Include some support code for loading pngs.
 #include "halide_image_io.h"
 using namespace Halide;
 using namespace Halide::Tools;
 using namespace std;
 
-#define DO_ASSERT
+//#define DO_ASSERT
 
 Halide::Image<uint32_t> sumImageRow(Halide::Image<uint8_t>& input)
 {
@@ -21,11 +22,9 @@ Halide::Image<uint32_t> sumImageRow(Halide::Image<uint8_t>& input)
 	Func f;
 	RDom r(0, input.width());
 	f(y, c) = sum(cast<uint32_t>(input(r, y, c)));
-	f.trace_stores();
 	f.parallel(y);
 	//f.trace_stores();
 	Halide::Image<uint32_t> output = f.realize(input.height(), 3);
-
 
 #ifdef DO_ASSERT
 	// assert correctness
@@ -51,19 +50,94 @@ Halide::Image<uint32_t> sumImageRow(Halide::Image<uint8_t>& input)
 }
 
 
+tuple<int, int> findHeadAndTail(const Halide::Image<uint32_t>& sum1, const Halide::Image<uint32_t>& sum2)
+{
+	int width = min(sum1.width(), sum2.width());
+	int head = 0, tail = width -1;
+	for (; head < width; ++head)
+	{
+		bool is_equal = true;
+		for (int i = 0; i < 3; ++i)
+		{
+			if (sum1(head, i) != sum2(head, i))
+			{
+				is_equal = false;
+				break;
+			}
+		}
+		
+		if (!is_equal)
+		{
+			break;
+		}
+	}
+
+	for (; tail >= 0; --tail)
+	{
+		bool is_equal = true;
+		for (int i = 0; i < 3; ++i)
+		{
+			if (sum1(tail, i) != sum2(tail, i))
+			{
+				is_equal = false;
+				break;
+			}
+		}
+
+		if (!is_equal)
+		{
+			++tail;
+			break;
+		}
+	}
+
+	cout << width << ", " << head << ", " << tail << endl;
+
+	return tuple<int, int>(head, tail);
+}
+
+Halide::Image<uint8_t> cutHeadAndTail(const Halide::Image<uint8_t>& input, int head, int tail)
+{
+	Var x("x"), y("y"), c("c");
+	RDom r(0, tail - head);
+	Func f;
+	f(x, y, c) = input(x, y + head, c);
+	Halide::Image<uint8_t> output = f.realize(input.width(), tail - head, input.channels());
+	return output;
+}
+
 int main(int argc, char **argv) {
 	Halide::Image<uint8_t> input[3];
+	Halide::Image<uint32_t> sums[3];
 	char filename[256];
 	for (int i = 0; i < 3; ++i)
 	{
 		sprintf_s(filename, "pics/%d.png", i+1);
 		input[i] = load_image(filename);
+		sums[i] = sumImageRow(input[i]);
 	}
 
-	auto sum1 = sumImageRow(input[0]);
-	printf("%d, %d\n", sum1.width(), sum1.height());
+// 	for (int i = 0; i < 10; ++i)
+// 	{
+// 		printf("sums[0][%d] = (%d, %d, %d)\n", i, sums[0](i, 0), sums[0](i, 1), sums[0](i, 2));
+// 		printf("sums[1][%d] = (%d, %d, %d)\n", i, sums[1](i, 0), sums[1](i, 1), sums[1](i, 2));
+// 	}
+
+	auto ht = findHeadAndTail(sums[0], sums[1]);
+
+	int head = get<0>(ht), tail = get<1>(ht);
+	cout << get<0>(ht) << ", " << get<1>(ht) << endl;
+
+	assert(tail > head);
+
+	auto out0 = cutHeadAndTail(input[0], head, tail);
+	save_image(out0, "out0.png");
+
+	auto out1 = cutHeadAndTail(input[1], head, tail);
+	save_image(out1, "out1.png");
 
 	cout << "Success!" << endl;
 
 	return 0;
 }
+
