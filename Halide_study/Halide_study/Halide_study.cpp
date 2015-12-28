7e01e803997d10764cf1ab0ce4799dae773d0990
legaldev <legaldev0@gmail.com>
2015-12-28 21:24:30 +0800

    try to optimize the cost time by calc sum of block image

--- Halide_study/Halide_study/Halide_study.cpp
+++ Halide_study/Halide_study/Halide_study.cpp
@@ -6,6 +6,8 @@
 #include "Halide.h"
 #include <iostream>
 #include <stdio.h>
+#include <ctime>
+#include <vector>
 
 
 // Include some support code for loading pngs.
@@ -14,6 +16,7 @@ using namespace Halide;
 using namespace Halide::Tools;
 using namespace std;
 
+#define PV(v) std::cout << #v << " = " << v << std::endl;
 
 //#define DO_ASSERT
 
@@ -61,7 +64,7 @@ Halide::Image<uint32_t> sumImageRowWithFilter(const Halide::Image<uint8_t>& inpu
 	//f.trace_stores();
 	Halide::Image<uint32_t> output = f.realize(3, input.height());
 
-//#ifdef DO_ASSERT
+#ifdef DO_ASSERT
 	// assert correctness
 	for (int j = 0; j < input.height(); j++)
 	{
@@ -70,7 +73,7 @@ Halide::Image<uint32_t> sumImageRowWithFilter(const Halide::Image<uint8_t>& inpu
 		{
 			for (int k = 0; k < 3; k++)
 			{
-				c[k] += input(i, j, k) * filter(i, j);
+				c[k] += input(i, j, k);
 			}
 		}
 		for (int k = 0; k < 3; k++)
@@ -79,7 +82,53 @@ Halide::Image<uint32_t> sumImageRowWithFilter(const Halide::Image<uint8_t>& inpu
 			assert(c[k] == output(k, j));
 		}
 	}
-//#endif // DO_ASSERT
+#endif // DO_ASSERT
+
+	return output;
+}
+
+Halide::Image<uint32_t> sumImageRowBlock(const Halide::Image<uint8_t>& input, int block)
+{
+	Var x("x"), y("y"), c("c");
+	Func f("block");
+	f(x, y, c) = cast<uint32_t>(0);
+	int block_width = input.width() / block;
+	RDom r(0, block_width);
+	for (int i = 0; i < block-1; ++i)
+	{
+		f(i, y, c) = sum(cast<uint32_t>(input(block_width * i + r, y, c)));
+	}
+	RDom r2(block_width * (block-1), input.width() - block_width * (block - 1));
+	f(block - 1, y, c) = sum(cast<uint32_t>(input(r2, y, c)));
+
+	//f.parallel(y);
+	//f.trace_stores();
+	Halide::Image<uint32_t> output = f.realize(block, input.height(), 3);
+	cout << "block done" << endl;
+
+#ifdef DO_ASSERT
+	// assert correctness
+	for (int j = 0; j < input.height(); j++)
+	{
+		for (int b = 0; b < block - 1; ++b)
+		{
+			uint32_t c[3] = { 0 };
+			for (int i = b*block_width; i < block_width*(b + 1); ++i)
+			{
+				for (int k = 0; k < 3; k++)
+				{
+					c[k] += input(i, j, k);
+				}
+			}
+			for (int k = 0; k < 3; k++)
+			{
+				//printf("(%d, %d) = %d, %d\n", j, k, c[k], output(k, j));
+				assert(c[k] == output(b, j, k));
+			}
+		}
+
+	}
+#endif // DO_ASSERT
 
 	return output;
 }
@@ -186,10 +235,10 @@ int matchImages(const Halide::Image<uint32_t>& top, const Halide::Image<uint32_t
 	return res;
 }
 
-
 // calc average match of each row
 // backgroud color will be filtered
-int avgMatchImages(const Halide::Image<uint8_t>& top, const Halide::Image<uint8_t>& down)
+template<typename T>
+int avgMatchImages(const Halide::Image<T>& top, const Halide::Image<T>& down)
 {
 	const int width = min(top.width(), down.width()), height = min(top.height(), down.height());
 
@@ -213,7 +262,7 @@ int avgMatchImages(const Halide::Image<uint8_t>& top, const Halide::Image<uint8_
 			
 		}
 		float avgm = float(match) / (h * width);
-		if (avgm > maxm)
+		if (avgm >= maxm)
 		{
 			maxm = avgm;
 			res = max(res, h);
@@ -257,6 +306,9 @@ int matchImages(const Halide::Image<uint8_t>& top, const Halide::Image<uint8_t>&
 }
 
 int main(int argc, char **argv) {
+	clock_t begin = clock(), end = 0;
+	float elapsed_time = 0;
+
 	const int num = 3;
 	Halide::Image<uint8_t> input[num];
 	Halide::Image<uint32_t> sums[num];
@@ -268,6 +320,11 @@ int main(int argc, char **argv) {
 		sums[i] = sumImageRow(input[i]);
 	}
 
+	end = clock();
+	elapsed_time = float(end - begin) / CLOCKS_PER_SEC;
+	printf("load time = %f\n", elapsed_time);
+	begin = clock();
+
 	const int width = input[0].width(), height = input[0].height(), channel=input[0].channels();
 
 	int head = 0, tail = 0;
@@ -277,12 +334,16 @@ int main(int argc, char **argv) {
 
 		head = max(head, get<0>(ht));
 		tail = max(tail, get<1>(ht));
-
 	}
 
 	printf("head = %d, tail = %d\n", head, tail);
 
 	assert(tail + head < height);
+
+	end = clock();
+	elapsed_time = float(end - begin) / CLOCKS_PER_SEC;
+	printf("findHeadAndTail time = %f\n", elapsed_time);
+	begin = clock();
 
 	// cut head and tail
 	Halide::Image<uint8_t> cuts[num];
@@ -290,22 +351,33 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < num; ++i)
 	{
 		cuts[i] = cutHeadAndTail(input[i], head, tail);
-		cut_sums[i] = cutHeadAndTail(sums[i], head, tail);
+		//cut_sums[i] = cutHeadAndTail(sums[i], head, tail);
+		cut_sums[i] = sumImageRowBlock(cuts[i], input[i].width());
 
 		sprintf_s(filename, "out%d.png", i);
 		save_image(cuts[i], filename);
 	}
+
+	end = clock();
+	elapsed_time = float(end - begin) / CLOCKS_PER_SEC;
+	printf("cutHeadAndTail time = %f\n", elapsed_time);
+	begin = clock();
 
 	// find match bwtween cuts
 	int match[num - 1] = { 0 };
 	int matchall = 0;
 	for (int i = 0; i < num; ++i)
 	{
-		match[i] = avgMatchImages(cuts[i], cuts[i + 1]);
+		//match[i] = avgMatchImages(cuts[i], cuts[i + 1]);
+		match[i] = avgMatchImages(cut_sums[i], cut_sums[i + 1]);
 		matchall += match[i];
 		cout << "match = " << match[i] << endl;
 	}
-// 	exit(0);
+
+	end = clock();
+	elapsed_time = float(end - begin) / CLOCKS_PER_SEC;
+	printf("avgMatchImages time = %f\n", elapsed_time);
+	begin = clock();
 
 	// joint all the cut images
 	Func joint("joint");
@@ -336,8 +408,12 @@ int main(int argc, char **argv) {
 	Halide::Image<uint8_t> res = joint.realize(res_width, res_height, channel);
 	save_image(res, "res.png");
 
-
 	cout << "Success!" << endl;
+
+	end = clock();
+	elapsed_time = float(end - begin) / CLOCKS_PER_SEC;
+	printf("joint time = %f\n", elapsed_time);
+	begin = clock();
 
 	return 0;
 }
